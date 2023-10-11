5c0e7be6834b8fae0d64069b7675a163addcf1ae
vladsigopka <vlad.tereshhenko.00@mail.ru>
2023-10-12 00:17:02 +0700

    добавление 4 колонки и вывод на сайт

--- js/main.js
+++ js/main.js
@@ -228,3 +228,108 @@ Vue.component('column_3', {
         }
     }
 })
+Vue.component('column_4', {
+    template: `
+        <section id="main" class="main-alt">
+            <div class="column column__four">
+            <p>Выполненные задачи</p>
+                <div class="card" v-for="card in column_4">
+                    <div class="tasks">Название: {{ card.name }}</div>
+                    <div class="tasks">Описание: {{ card.description }}</div>
+                    <div class="tasks">Дата создания: {{ card.date }}</div>
+                    <div class="tasks">Крайний срок: {{ card.deadline }}</div>
+                        <div class="tasks" v-if="card.period" style="color: green">Завершено вовремя</div>
+                        <div class="tasks" v-else style="color: red">Завершено не вовремя</div>
+                </div>
+            </div>
+        </section>
+    `,
+    props: {
+        column_4: {
+            type: Array,
+        },
+        card: {
+            type: Object
+        }
+    },
+})
+Vue.component('newCard', {
+    template: `
+<section>
+<a href="#openModal" class="btn btnModal">Создать карточку</a>
+<div id="openModal" class="modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title">Название</h3>
+        <a href="#close" title="Close" class="close">×</a>
+      </div>
+      <div class="modal-body">    
+    <div class="addForm">
+        <form @submit.prevent="onSubmit">
+            <div class="form__control">
+                <div class="form__name field">
+
+                    <input id="point" required v-model="name" type="text" placeholder="Название">
+                </div>
+            <div class="field">
+
+                <textarea required id="point" v-model="description" placeholder="Описание"> </textarea>
+            </div>
+            <div class="field">
+
+                <input required type="date" id="point" v-model="deadline">
+            </div>
+            <button type="submit" class="btn" @click="persist">Добавить</button>
+            </div>
+        </form>
+    </div>
+          </div>
+    </div>
+  </div>
+</div>
+</section>
+`,
+    data() {
+        return {
+            name: null,
+            description: null,
+            date: null,
+            deadline: null
+        }
+    },
+    methods: {
+        onSubmit() {
+            let card = {
+                name: this.name,
+                description: this.description,
+                date: new Date().toLocaleDateString().split(".").reverse().join("-"),
+                deadline: this.deadline,
+                reason: [],
+                transfer: false,
+                edit: false,
+                editDate: null,
+                period: true
+            }
+            eventBus.$emit('addColumn_1', card)
+            this.name = null
+            this.description = null
+            this.date = null
+            this.deadline = null
+        },
+        persist() {
+            localStorage.name = this.name;
+            localStorage.description = this.description;
+            localStorage.deadline = this.deadline;
+        }
+    },
+    mounted() {
+        if(localStorage.name) this.name = localStorage.name;
+        if(localStorage.description) this.description = localStorage.description;
+        if(localStorage.deadline) this.deadline = localStorage.deadline;
+    },
+})
+
+let app = new Vue({
+    el: '#app',
+})
